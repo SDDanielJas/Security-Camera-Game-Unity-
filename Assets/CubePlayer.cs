9a3c6c56a0d62e7c4ef2f99c2b49c792c74dcacb
Daniel Jasinski <79961890+SDDanielJas@users.noreply.github.com>
2021-05-03 15:13:46 -0500

    Add files via upload

---
+++ Assets/CubePlayer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+using TMPro;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CubePlayer : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+
+    [SerializeField]
+    TMP_Text AttemptCounter;
+
+    [SerializeField]
+    TMP_Text Timer;
+
+
+
+    float StartTime;  
+  
+    
+    public static int Attempts = 0; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+        StartTime = Time.time;
+    }
+
+    void Update () { 
+        AttemptCounter.text = "Attempts: " + Attempts; 
+        float t = Time.time - StartTime; 
+        string minute = ((int) t / 60).ToString();
+        string secound = (t % 60).ToString("f2");
+
+        Timer.text = minute + ":" + secound;
+
+    }
+    // Update is called once per frame
+    void FixedUpdate() 
+    {
+        rb.AddForce(Input.GetAxis("Horizontal") * 50, 0, Input.GetAxis("Vertical") *50); 
+    }
+
+    void OnTriggerEnter (Collider other) {
+        if(other.gameObject.CompareTag("EnemyReset")) {
+            UnityEngine.Debug.Log("I've been hit");
+            ++Attempts;
+            Destroy(this.gameObject); 
+            SceneManager.LoadScene("SampleScene");
+        }
+
+            if (other.gameObject.CompareTag("Winner")) {
+                Debug.Log("I have hit the unity scene");
+                 Time.timeScale = 0;
+            }
+    }
+}
---
+++ Assets/SecurityCamera.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCamera : MonoBehaviour
+{
+    public Transform rayEmitter; 
+
+    private RaycastHit hit; 
+    private Renderer rend; 
+
+    bool canshoot = true; 
+
+    [SerializeField]
+    Transform GameObject; 
+
+    [SerializeField]
+    Rigidbody Sphere; 
+
+    void Start() {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics.Raycast(rayEmitter.position, transform.forward, out hit)) {
+            if(hit.collider.CompareTag("Player")) {
+                rend.material.color = Color.red; 
+                Debug.Log("I have hit");
+                if (canshoot == true) {  
+                
+                Rigidbody bullet = Instantiate(Sphere, GameObject.position, GameObject.rotation); 
+                bullet.AddForce(transform.forward*30, ForceMode.Impulse);
+                canshoot = false; 
+                 StartCoroutine(Wait());
+                } 
+            } else {
+                rend.material.color = Color.green;
+                Debug.DrawRay(rayEmitter.position, transform.forward *10, Color.white, 1); 
+            }
+        }
+         Debug.DrawRay(rayEmitter.position, transform.forward *10, Color.white, 1); 
+
+    }
+
+    IEnumerator Wait() {
+        yield return new WaitForSeconds(1);
+        canshoot = true;
+    }
+}
